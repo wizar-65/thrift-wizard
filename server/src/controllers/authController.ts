e66c70a3d651727494e4c3418381f4cb18bdc0b8
tateweaver <tateweaver23@gmail.com>
2025-04-16 21:37:04 -0400

    login endpoint working , need to store access + refresh token appropriately

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -6,7 +6,6 @@ import { generateAccessToken, generateRefreshToken } from "../utils/tokenUtils"
 import pool from "../models/db"
 
 // these need to go in the DB
-const users: User[] = []
 let refreshTokens: string[] = []
 
 export function token(req: Request, res: Response) {
@@ -68,30 +67,38 @@ export async function register(req: Request, res: Response) {
 
 export async function login(req: Request, res: Response) {
   // Authenticate User
-  const user = users.find((user) => user.username === req.body.username)
-  if (user == null) {
-    res.status(400).send("Cannot find user")
-    return
-  }
+  const { username, password } = req.body
   try {
-    if (!(await bcrypt.compare(req.body.password, user.password))) {
-      res.send("Invalid Credentials")
+    const userQuery = await pool.query(
+      "SELECT * FROM public.users WHERE username = $1",
+      [username]
+    )
+    if (userQuery.rows.length === 0) {
+      res.status(400).send({ message: "Invalid username or password" })
       return
     }
-  } catch {
-    res.send("Error")
+
+    const user = userQuery.rows[0]
+
+    if (!(await bcrypt.compare(password, user.password_hash))) {
+      res.send("Invalid username or password")
+      return
+    }
+
+    const secretKey = process.env.ACCESS_TOKEN_SECRET
+    if (!secretKey) {
+      throw new Error("Server configuration error: Missing JWT secret")
+    }
+
+    const accessToken = generateAccessToken(user.username)
+    const refreshToken = generateRefreshToken(user.username)
+    refreshTokens.push(refreshToken)
+
+    res.json({ accessToken, refreshToken })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({ message: "Server error" })
   }
-
-  const secretKey = process.env.ACCESS_TOKEN_SECRET
-  if (!secretKey) {
-    throw new Error("Server configuration error: Missing JWT secret")
-  }
-
-  const accessToken = generateAccessToken(user.username)
-  const refreshToken = generateRefreshToken(user.username)
-  refreshTokens.push(refreshToken)
-
-  res.json({ accessToken, refreshToken })
 }
 
 export function logout(req: Request, res: Response) {
